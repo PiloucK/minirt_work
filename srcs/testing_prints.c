15602329554d18dc2d6af5a400dfe40d11b0c116
kuzniewycz <clkuznie@student.42.fr>
2020-09-02 23:34:10 +0200

    .

--- srcs/testing_prints.c
+++ srcs/testing_prints.c
@@ -6,7 +6,7 @@
 /*   By: clkuznie <clkuznie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/06 15:48:45 by clkuznie          #+#    #+#             */
-/*   Updated: 2020/08/30 02:31:06 by clkuznie         ###   ########.fr       */
+/*   Updated: 2020/09/02 21:29:40 by clkuznie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,5 @@
 void
     print_infos(t_info *info)
 {
-    ft_printf("%p\n", info);
-    ft_printf("    mlx_ptr = %p\n\
-    win ptr = %p\n\
-    win x size = %i\n\
-    win y size = %i\n\
-    t_elem_lst first elem = %p\n\
-    do_save = %i\n\
-    char *last_read_str = %s\n",
-    info->mlx,
-    info->win,
-    info->res->x_size,
-    info->res->y_size,
-    info->first_elem,
-    info->do_save,
-    info->last_read_str);
-    ft_printf("side info:\n\
-    res status = %i\n\
-    ambiant status = %i\n\n",
-    !!info->res,
-    !!info->ambiant);
+    (void)info;
 }
